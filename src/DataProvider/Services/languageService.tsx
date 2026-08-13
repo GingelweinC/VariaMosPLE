03e3da148385eb166d9ee9d8d2f41bc92c5444fc
Steluro <282664784+Steluro@users.noreply.github.com>
2026-08-13 13:01:00 -0400

    patch(reifications): add endpoints to languageService getters and fix type

--- src/DataProvider/Services/languageService.tsx
+++ src/DataProvider/Services/languageService.tsx
@@ -30,6 +30,10 @@ export default class LanguageService {
     try {
       const res = await LANGUAGES_CLIENT.get("/" + languageId + "/reification-types");
       const reifications: any = res.data;
+      await reifications.forEach(async reification => {
+        const res = await LANGUAGES_CLIENT.get("/" + languageId + "/reification-types/" + reification.uuid + "/endpoints");
+        reification.endpoints = res.data;
+      });
       return reifications;
     } catch (error) {
       console.log("Something wrong in getReificationsByLanguageId Service:", error);
--- src/Domain/ProductLineEngineering/Entities/Reification.ts
+++ src/Domain/ProductLineEngineering/Entities/Reification.ts
@@ -4,7 +4,7 @@ export class Reification {
   id: string = crypto.randomUUID();
   name: string;
   typeId: string;
-  endpoints: Record<string, string[]> = {};
+  endpoints: any[] = [];
   properties: Property[];
   x: number = 0;
   y: number = 0;
@@ -15,11 +15,13 @@ export class Reification {
     name: string,
     typeId: string,
     properties: Property[] = [],
+    endpoints: any[] = [],
     parentId: string = null,
   ) {
     this.name = name;
     this.typeId = typeId;
     this.properties = properties;
+    this.endpoints = endpoints;
     this.parentId = parentId;
   }
 }
