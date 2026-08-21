e1a17f66dc3d8c7bcbe9355152aba61025ad14e7
Steluro <282664784+Steluro@users.noreply.github.com>
2026-08-20 23:40:50 -0400

    style-fix graph-header

--- src/UI/GraphEditor/GraphHeader.tsx
+++ src/UI/GraphEditor/GraphHeader.tsx
@@ -1,3 +1,5 @@
+import "./GraphHeader.css";
+
 import ProjectService from "../../Application/Project/ProjectService";
 import { RiSave3Fill } from "react-icons/ri";
 import { FaRegFolderOpen, FaHistory } from "react-icons/fa";
@@ -6,60 +8,71 @@ import { BsChatLeftTextFill, BsFillClipboardFill } from "react-icons/bs";
 import { toPng } from "html-to-image";
 import { getNodesBounds, getViewportForBounds } from "@xyflow/react";
 import { Node } from "@xyflow/react";
+import { Button, ButtonGroup } from "react-bootstrap";
 
 type GraphHeaderProps = {
-    projectService: ProjectService;
-    nodes: Node[];
-    openHistoryPanel: () => void;
-    openAnnotationPanel: () => void;
+  projectService: ProjectService;
+  nodes: Node[];
+  openHistoryPanel: () => void;
+  openAnnotationPanel: () => void;
 };
 
-export function GraphHeader( { projectService, nodes, openHistoryPanel, openAnnotationPanel }: GraphHeaderProps ) {
-
+export function GraphHeader({
+  projectService,
+  nodes,
+  openHistoryPanel,
+  openAnnotationPanel,
+}: GraphHeaderProps) {
   async function btnDownloadImage_onClick() {
-      const viewport = document.querySelector(".react-flow__viewport") as HTMLElement | null;
-      if (!viewport || nodes.length === 0) {
-        return;
-      }
+    const viewport = document.querySelector(
+      ".react-flow__viewport",
+    ) as HTMLElement | null;
+    if (!viewport || nodes.length === 0) {
+      return;
+    }
 
-      const bounds = getNodesBounds(nodes);
-      const padding = 50;
-      const imageWidth = bounds.width + padding * 2;
-      const imageHeight = bounds.height + padding * 2;
-      const transform =getViewportForBounds(bounds, imageWidth, imageHeight, 0.1, 2, padding);
+    const bounds = getNodesBounds(nodes);
+    const padding = 50;
+    const imageWidth = bounds.width + padding * 2;
+    const imageHeight = bounds.height + padding * 2;
+    const transform = getViewportForBounds(
+      bounds,
+      imageWidth,
+      imageHeight,
+      0.1,
+      2,
+      padding,
+    );
 
-      try {
-        const dataUrl =
-          await toPng(
-            viewport,
-            {
-              backgroundColor:
-                "#ffffff",
-              width: imageWidth,
-              height: imageHeight,
-              pixelRatio: 2,
-              style: {
-                width: `${imageWidth}px`,
-                height: `${imageHeight}px`,
-                transform:
-                  `translate(${transform.x}px, ${transform.y}px) scale(${transform.zoom})`,
-              },
-            }
-          );
+    try {
+      const dataUrl = await toPng(viewport, {
+        backgroundColor: "#ffffff",
+        width: imageWidth,
+        height: imageHeight,
+        pixelRatio: 2,
+        style: {
+          width: `${imageWidth}px`,
+          height: `${imageHeight}px`,
+          transform: `translate(${transform.x}px, ${transform.y}px) scale(${transform.zoom})`,
+        },
+      });
 
-        const link = document.createElement("a");
+      const link = document.createElement("a");
 
-        link.download ="graph.png";
-        link.href = dataUrl;
-        link.click();
-      } catch (error) {
-        console.error("Erreur lors de l'export du graphe :", error);
-      }
-    };
+      link.download = "graph.png";
+      link.href = dataUrl;
+      link.click();
+    } catch (error) {
+      console.error("Erreur lors de l'export du graphe :", error);
+    }
+  }
 
   function btnSaveConfiguration_onClick() {
     try {
-      projectService.raiseEventRequestSaveConfigurationListener(projectService.project, projectService.currentModel.id);
+      projectService.raiseEventRequestSaveConfigurationListener(
+        projectService.project,
+        projectService.currentModel.id,
+      );
     } catch (ex) {
       alert(JSON.stringify(ex));
     }
@@ -67,7 +80,10 @@ export function GraphHeader( { projectService, nodes, openHistoryPanel, openAnno
 
   function btnOpenConfiguration_onClick() {
     try {
-      projectService.raiseEventRequestOpenConfigurationListener(projectService.project,projectService.currentModel.id);
+      projectService.raiseEventRequestOpenConfigurationListener(
+        projectService.project,
+        projectService.currentModel.id,
+      );
     } catch (ex) {
       alert(JSON.stringify(ex));
     }
@@ -75,7 +91,7 @@ export function GraphHeader( { projectService, nodes, openHistoryPanel, openAnno
 
   function btnResetConfiguration_onClick() {
     try {
-      if (window.confirm( "Do you really want to reset the configuration?")) {
+      if (window.confirm("Do you really want to reset the configuration?")) {
         projectService.resetConfiguration(projectService.currentModel);
       }
     } catch (ex) {
@@ -100,38 +116,82 @@ export function GraphHeader( { projectService, nodes, openHistoryPanel, openAnno
   }
 
   return (
-    <div className="header">
-        <button title="Download image"onClick={btnDownloadImage_onClick}>
-          <i className="bi bi-card-image" />
-        </button>
+    <ButtonGroup className="graph-header">
+      <Button
+        variant="outline-secondary"
+        title="Download image"
+        onClick={btnDownloadImage_onClick}
+      >
+        <i className="bi bi-card-image" />
+      </Button>
 
-        <button title="Save configuration" onClick={btnSaveConfiguration_onClick}>
-          <span> <RiSave3Fill /> </span>
-        </button>
+      <Button
+        variant="outline-secondary"
+        title="Save configuration"
+        onClick={btnSaveConfiguration_onClick}
+      >
+        <span>
+          <RiSave3Fill />
+        </span>
+      </Button>
 
-        <button title="Load configuration" onClick={btnOpenConfiguration_onClick}>
-          <span> <FaRegFolderOpen /> </span>
-        </button>
+      <Button
+        variant="outline-secondary"
+        title="Load configuration"
+        onClick={btnOpenConfiguration_onClick}
+      >
+        <span>
+          <FaRegFolderOpen />
+        </span>
+      </Button>
 
-        <button title="Reset configuration" onClick={btnResetConfiguration_onClick}>
-          <span> <FaBolt /></span>
-        </button>
+      <Button
+        variant="outline-secondary"
+        title="Reset configuration"
+        onClick={btnResetConfiguration_onClick}
+      >
+        <span>
+          <FaBolt />
+        </span>
+      </Button>
 
-        <button title="Draw core" onClick={btnDrawCoreFeatureTree_onClick}>
-          <span>C</span>
-        </button>
+      <Button
+        variant="outline-secondary"
+        title="Draw core"
+        onClick={btnDrawCoreFeatureTree_onClick}
+      >
+        <span>C</span>
+      </Button>
 
-        <button title="Copy model configuration" onClick={btnCopyModelConfiguration_onClick}>
-          <span> <BsFillClipboardFill /> </span>
-        </button>
+      <Button
+        variant="outline-secondary"
+        title="Copy model configuration"
+        onClick={btnCopyModelConfiguration_onClick}
+      >
+        <span>
+          <BsFillClipboardFill />
+        </span>
+      </Button>
 
-        <button title="History" onClick={openHistoryPanel} style={{marginLeft: "6px"}}>
-          <span> <FaHistory /> </span>
-        </button>
+      <Button
+        variant="outline-secondary"
+        title="History"
+        onClick={openHistoryPanel}
+      >
+        <span>
+          <FaHistory />
+        </span>
+      </Button>
 
-        <button title="Comments" onClick={openAnnotationPanel} style={{marginLeft: "6px"}}>
-          <span> <BsChatLeftTextFill /> </span>
-        </button>
-      </div>
-    );
+      <Button
+        variant="outline-secondary"
+        title="Comments"
+        onClick={openAnnotationPanel}
+      >
+        <span>
+          <BsChatLeftTextFill />
+        </span>
+      </Button>
+    </ButtonGroup>
+  );
 }
